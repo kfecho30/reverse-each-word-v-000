e5099d0485cea3340231d6fdf6a4edf8d488df06
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 18:01:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-  arr = string.split(" ")
-  med = []
-  arr.each do |a|
-    med << a.reverse
-  end
-  final = med.join(" ")
-  final
-end
+#def reverse_each_word(string)
+#  arr = string.split(" ")
+#  med = []
+#  arr.each do |a|
+#    med << a.reverse
+#  end
+#  final = med.join(" ")
+#  final
+#end
 
 def reverse_each_word(string)
   arr = string.split(" ")
